184c8efd92bb2cb6e30aada428311f7283f8a545
MelvinLim9913 <melvin@Melvins-MacBook-Pro.local>
2021-04-04 15:50:10 +0800

    use cpu only for validation

--- trainer.py
+++ trainer.py
@@ -124,7 +124,8 @@ class Classifier:
         for img, label in tqdm.tqdm(valid_dl):
             # img = img.to(self.device)
             # label = label.to(self.device)
-
+            self.criterion.to(torch.device('cpu'))
+            
             with torch.no_grad():
                 logits = model(img)
                 loss = self.criterion(logits, label)
