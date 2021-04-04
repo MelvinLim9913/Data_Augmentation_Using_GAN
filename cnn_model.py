8329613c4c8f9f697a9fc085075d503393b10560
MelvinLim9913 <melvin@Melvins-MacBook-Pro.local>
2021-04-04 14:20:53 +0800

    Fix pretrained model

--- cnn_model.py
+++ cnn_model.py
@@ -9,17 +9,18 @@ class CNNModel(nn.Module):
         # Load pretrained network as backbone
         pretrained = get_model(model_name, pretrained=True)
         # remove last layer of fc
-        self.backbone = pretrained.features
-        self.output = pretrained.output
+        self.backbone = pretrained
+        self.num_ftrs = pretrained.fc.in_features
+        # self.output = pretrained.output
         # self.dropout1 = nn.Dropout(0.4)
-        self.classifier = nn.Linear(1000, 7)
+        self.classifier = nn.Linear(self.num_ftrs, 7)
 
         del pretrained
 
     def forward(self, x):
         x = self.backbone(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.output(x)
+        # x = x.reshape(x.size(0), -1)
+        # x = self.output(x)
         x = self.classifier(x)
         return x
 
