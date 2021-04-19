8eb29fe50341d5bd476d1d9976a57694ed0a8c15
MelvinLim9913 <melvin@Melvins-MacBook-Pro.local>
2021-04-19 19:00:25 +0800

    Modify noise image generator

--- noise_image_generator.py
+++ noise_image_generator.py
@@ -14,7 +14,7 @@ class NoiseImageGenerator:
             emotion_image_path = os.path.join(self.original_image_filepath, str(i))
             emotion_image_files = os.listdir(emotion_image_path)
             image_label_dict["Image"] += [os.path.join(emotion_image_path, path) for path in emotion_image_files]
-            image_label_dict["Label"] += [i for i in range(len(emotion_image_files))]
+            image_label_dict["Label"] += [0 for i in range(len(emotion_image_files))]
             image_label_df = pd.DataFrame(image_label_dict)
             print(image_label_df)
         return image_label_df
